17e409ef08269b1219b757afd52a7b09dd387485
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-10-03 09:01:51 -0300

    refactor: alter server to use routes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import { config } from "dotenv";
+import express from 'express';
+import {config} from 'dotenv';
 
-config();
+import routesfro from './routes/index.routes.js';
+import routes from './routes/index.routes.js';
 
-const serverPort = process.env.PORT || 3000;
+config()
+
+const serverPort = process.env.PORT || 4000
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
+app.use (express.json());
+app.use (routes);
 
 app.listen(serverPort, () => {
-  console.log(`⚡ Server started on http://localhost:${serverPort}`);
-});
+    console.log(`💜 Server started on http://localhost:${serverPort}`)
+})
